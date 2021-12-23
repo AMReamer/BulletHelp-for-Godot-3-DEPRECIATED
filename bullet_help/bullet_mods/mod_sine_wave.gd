44eaec7e9fb2fcb6040cb7dd2c43eef30f489508
AMReamer <amreamer277@gmail.com>
2021-12-23 11:30:23 -0500

    Update mod_sine_wave.gd
    Added across velocity option to mod_sin_wave

--- bullet_help/bullet_mods/mod_sine_wave.gd
+++ bullet_help/bullet_mods/mod_sine_wave.gd
@@ -5,8 +5,12 @@ extends ModSine
 
 
 export (Vector2) var axis := Vector2(0, 1) # Axis wave oscillates upon
+export (bool) var along_velocity := true # Have it oscillate perpendicular to velocity as the axis
 
 
 # Changes position
 func update_value(change):
+	if (along_velocity):
+		axis = Vector2(-target.velocity.y, target.velocity.x).normalized()
+	
 	target.position += axis * change
